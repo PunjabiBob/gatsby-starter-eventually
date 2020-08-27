39cdf632f3d214f0d04541f1ea0fb90de202c118
PunjabiBob <bob@seapointadvisors.com>
2020-08-26 18:44:54 -0700

    add US currency

--- config.js
+++ config.js
@@ -7,11 +7,11 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/gatsby-starter-eventually/`, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: `/gatsby-starter-eventually/`, 
   // social
-  heading: 'For Sale - VoipEquipment.com - $3,900',
+  heading: 'For Sale - VoipEquipment.com - US$3,900',
   subHeading:
-    "This web domain is for sale. Buy VoipEquipment.com for $3,900.",
+    "This web domain is for sale. Buy VoipEquipment.com for US$3,900.",
   socialLinks: [
     {
  
